0977d4c29b656b3a4cb22362d4a1d45985b3ab40
Andres Viedma <andres.viedma@gmail.com>
2025-07-22 01:45:06 +0200

    JacksonObjectMapper: Use default object mapper

--- serialization/poket-jackson/src/main/kotlin/io/github/andresviedma/poket/support/serialization/jackson/JacksonObjectMapper.kt
+++ serialization/poket-jackson/src/main/kotlin/io/github/andresviedma/poket/support/serialization/jackson/JacksonObjectMapper.kt
@@ -1,13 +1,13 @@
 package io.github.andresviedma.poket.support.serialization.jackson
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.github.andresviedma.poket.config.propertytree.MapObjectMapper
+import io.github.andresviedma.poket.support.serialization.jackson.DefaultJacksonMappers.DEFAULT_JACKSON_SERIALIZER
 import kotlin.reflect.KClass
 
 class JacksonObjectMapper : MapObjectMapper {
-    private val objectMapper: ObjectMapper = jacksonObjectMapper()
+    private val objectMapper: ObjectMapper = DEFAULT_JACKSON_SERIALIZER
 
     override fun objectToMap(configClass: KClass<*>, obj: Any?): Map<String, *>? {
         if (obj == null) return null
