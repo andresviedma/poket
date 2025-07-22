72d37df03f34b5235727e25bc20bfab41879ec02
Andres Viedma <andres.viedma@gmail.com>
2025-07-22 18:14:16 +0200

    Create function to create easily an ObjectCacheFactory with a single system and no injection

---
+++ poket-facade/src/main/kotlin/io/github/andresviedma/poket/cache/utils/CacheFactoryGenerator.kt
@@ -0,0 +1,44 @@
+package io.github.andresviedma.poket.cache.utils
+
+import io.github.andresviedma.poket.cache.CacheConfig
+import io.github.andresviedma.poket.cache.CacheMetrics
+import io.github.andresviedma.poket.cache.CacheSystem
+import io.github.andresviedma.poket.cache.CacheSystemProvider
+import io.github.andresviedma.poket.cache.CacheTypeConfig
+import io.github.andresviedma.poket.cache.ObjectCacheFactory
+import io.github.andresviedma.poket.config.utils.configWith
+import io.github.andresviedma.poket.mutex.DistributedMutexFactory
+import io.github.andresviedma.poket.mutex.LockSystem
+import io.github.andresviedma.poket.mutex.LockSystemProvider
+import io.github.andresviedma.poket.mutex.MutexConfig
+import io.github.andresviedma.poket.mutex.MutexTypeConfig
+import io.github.andresviedma.poket.mutex.local.LocalLockSystem
+
+fun createCacheFactoryWithSystem(
+    cacheSystem: CacheSystem,
+    lockSystem: LockSystem = LocalLockSystem(),
+    baseCacheConfig: CacheTypeConfig? = null,
+    baseMutexConfig: MutexTypeConfig? = null,
+): ObjectCacheFactory {
+    val config = configWith(
+        CacheConfig(
+            default = CacheTypeConfig(cacheSystem = cacheSystem.getId()).overriddenWith(baseCacheConfig),
+        ),
+        MutexConfig(
+            default = MutexTypeConfig(lockSystem = lockSystem.getId()).overriddenWith(baseMutexConfig),
+        ),
+    )
+    return ObjectCacheFactory(
+        mutexFactory = DistributedMutexFactory(
+            lockSystemProvider = LockSystemProvider.withLockSystems(lockSystem),
+            configProvider = config,
+        ),
+        cacheSystemProvider = CacheSystemProvider(
+            registeredSystems = setOf(cacheSystem),
+            cacheMetrics = CacheMetrics(),
+            configProvider = config,
+        ),
+        configProvider = config,
+        cacheMetrics = CacheMetrics(),
+    )
+}
